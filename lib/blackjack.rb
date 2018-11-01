137de8ac299009effd016da47561b8d96d08761c
Thompson Plyler <nealok.g@gmail.com>
2018-11-01 01:17:01 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -54,7 +54,7 @@ def runner
   card_total = initial_round
   
   until card_total > 21 do
-    card total = hit?(card_total) 
+    card_total = hit?(card_total) 
     display_card_total(card_total)
   end
 
